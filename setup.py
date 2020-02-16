36d4a0051da32e3a64647bfec1ff70b0bfa78545
Issac Goldstand <margol@beamartyr.net>
2020-02-16 19:33:59 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from codecs import open
+from os import path
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="pycoolmaster",
+    version="0.1.0",
+    description="Lightweight Python API for older (RS232-only) CoolMaster HVAC bridges",
+    long_description=long_description,
+    author="Issac Goldstand",
+    author_email="margol@beamartyr.net",
+    url="http://github.com/issacg/pycoolmaster",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Home Automation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires=[],
+    zip_safe=True,
+    keywords="hvac homeautomation",
+)
